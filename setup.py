f830e4bfde371843c2fe9b0289b96d693e31b835
Jordan Baker <jbb@scryent.com>
2009-08-05 16:17:38 -0400

    added simple setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='pybrightcove',
+      version=version,
+      description="",
+      long_description='',
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup']),
+#      namespace_packages=
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          ],
+      )
